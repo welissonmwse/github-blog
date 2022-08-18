110bdd236e53c8615575acc1ccaed989a93f6b3a
Welisson Silveira <welissonmwse@gmail.com>
2022-08-18 16:07:18 -0300

    Estrutura visual - Componente Search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,16 @@ export function Home(){
   return (
     <main className="max-w-4xl mx-auto mt-[-88px]">
       <Profile />
+      <header className="flex justify-between items-center mt-[72px]">
+        <h2 className="text-base-subtitle font-nunito text-lg font-bold">Publicações</h2>
+        <p className="font-nunito text-base-span font-normal text-sm">6 publicações</p>
+      </header>
+      <form className="mt-3" action="">
+        <input className="placeholder:font-nunito placeholder:text-base-label py-3 px-4 rounded-md w-full bg-base-input border border-base-border text-base-text" 
+          type="text" 
+          placeholder="Buscar conteúdo" 
+        />
+      </form>
     </main>
   )
 }
