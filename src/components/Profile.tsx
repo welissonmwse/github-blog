7e2ab51d70aaa16649582d7fdd0e12d47c7c3e2c
Welisson Silveira <welissonmwse@gmail.com>
2022-08-18 17:59:46 -0300

    Conexão com a API - Requisição HTTP buscando dados do User

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,36 +1,75 @@
+import { useEffect, useState } from 'react'
 import {FaExternalLinkAlt, FaBuilding, FaUserFriends, FaGithub} from 'react-icons/fa'
 
+interface ProfileDataProps {
+  name: string
+  bio: string | null
+  followers: number
+  company: string | null
+  userName: string
+  avatar: string
+}
+
 export function Profile() {
+  const [user, setUser] = useState<ProfileDataProps>()
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://api.github.com/users/welissonmwse')
+      const data = await response.json()
+      const newUser = {
+        name: data.name,
+        bio: data.bio,
+        followers: data.followers,
+        company: data.company,
+        userName: data.login,
+        avatar: data.avatar_url
+      }
+      setUser(newUser)
+    })()
+  }, [])
+
   return(
     <div className="flex items-center gap-8 p-8 rounded-[0.625rem] bg-base-profile">
       <div className="min-w-[148px] h-[148px] rounded-lg overflow-hidden">
-        <img className="object-cover w-full h-full" src="https://github.com/welissonmwse.png" alt="" />
+        <img className="object-cover w-full h-full" src={user?.avatar} alt="" />
       </div>
-      <div>
+      <div className="w-full min-h-full flex flex-col">
         <header className="flex justify-between">
-          <h1 className="text-base-title font-bold text-2xl font-nunito leading-8">Welisson Silveira</h1>
-          <a className="flex gap-2 items-center font-bold font-nunito text-xs text-blue" href="#">
+          <h1 className="text-base-title font-bold text-2xl font-nunito leading-8">{user?.name}</h1>
+          <a className="flex gap-2 items-center font-bold font-nunito text-xs text-blue" 
+            href={`https://github.com/${user?.userName}`}
+            target="_blank"
+          >
             GITHUB
             <FaExternalLinkAlt />
           </a>
         </header>
         <p className="font-nunito text-base-text font-normal text-base leading-6 mt-2">
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. 
-          Eu viverra massa quam dignissim aenean malesuada suscipit. 
-          Nunc, volutpat pulvinar vel mass.
+          {user?.bio}
         </p>
         <footer className="flex items-center gap-6 mt-6">
           <div className="flex items-center gap-2">
             <FaGithub className="text-base-label" />
-            <p className="font-nunito text-base-text font-normal text-base leading-6">cameronwll</p>
+            <p className="font-nunito text-base-text font-normal text-base leading-6">{user?.userName}</p>
           </div>
-          <div className="flex items-center gap-2">
-            <FaBuilding className="text-base-label" />
-            <p className="font-nunito text-base-text font-normal text-base leading-6">Rocketseat</p>
-          </div>
+          {user?.company && (
+            <div className="flex items-center gap-2">
+              <FaBuilding className="text-base-label" />
+              <p className="font-nunito text-base-text font-normal text-base leading-6">{user?.company}</p>
+            </div>
+          )}
           <div className="flex items-center gap-2">
             <FaUserFriends className="text-base-label" />
-            <p className="font-nunito text-base-text font-normal text-base leading-6">32 seguidores</p>
+              {user?.followers === 1 ? (
+                <p className="font-nunito text-base-text font-normal text-base leading-6">
+                  {user?.followers} seguidor
+                </p>
+              ) : (
+                <p className="font-nunito text-base-text font-normal text-base leading-6">
+                  {user?.followers} seguidores
+                </p>
+              )}
           </div>
         </footer>
       </div>
