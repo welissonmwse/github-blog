ae67051c71252e3dc58b8c4535816acce8f906dd
Welisson Silveira <welissonmwse@gmail.com>
2022-08-19 11:27:13 -0300

    Conexão com a API - Listando Posts

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,19 +1,34 @@
-export function PostCard(){
+import { formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
+import { NavLink } from "react-router-dom"
+
+interface PostsProps {
+  number: number
+  title: string
+  body: string
+  created_at: string
+}
+
+export function PostCard({number, title, body, created_at}:PostsProps){
+  const publishedDateRelativeNow = formatDistanceToNow(new Date(created_at),{
+    locale: ptBR,
+    addSuffix: true,
+  })
+  const paragraphs = body.split(/;?\r\n|;?\n|;?\r/)
+
   return (
-    <div className="rounded-[10px] p-8 bg-base-post">
+    <NavLink to={`/posts:${number}`} className="rounded-[10px] p-8 bg-base-post">
       <header className="flex gap-2 items-start">
         <h3 className="font-nunito text-xl font-bold text-base-title leading-8">
-          JavaScript data types and data structures
+          {title}
         </h3>
         <span className="text-base-span font-nunito text-sm font-normal whitespace-nowrap leading-8">
-          Há 1 dia
+          {publishedDateRelativeNow}
         </span>
       </header>
       <p className="mt-5 text-base-text font-nunito text-sm font-normal leading-[25.6px]">
-        Programming languages all have built-in data structures, 
-        but these often differ from one language to another. 
-        This article attempts to list the built-in data structures available in...
+        {paragraphs[0]}
       </p>
-    </div>
+    </NavLink>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import { PostCard } from "../components/PostCard";
 import { Profile } from "../components/Profile";
+import { api } from "../services/api";
+
+interface PostsDataProsp {
+  id: number
+  number: number
+  title: string
+  body: string
+  created_at: string
+}
 
 export function Home(){
+  const [posts, setPosts] = useState<PostsDataProsp[]>([])
+  // repo:rocketseat-education/reactjs-github-blog-challenge
+
+  useEffect(() => {
+    (async () => {
+      const  response  = await api.get('search/issues', {
+        params: {
+          q: 'repo:rocketseat-education/reactjs-github-blog-challenge'
+        }
+      })
+      
+      const postsData = response.data.items.map(post => {
+        return {
+          id: post.id,
+          number: post.number,
+          title: post.title,
+          body: post.body,
+          created_at: post.created_at
+        }
+      })
+      setPosts(postsData);
+      console.log(postsData);
+    })()
+  }, [])
+
   return (
     <main className="max-w-4xl mx-auto mt-[-88px]">
       <Profile />
@@ -16,15 +51,16 @@ export function Home(){
         />
       </form>
       <section className="grid grid-cols-2 gap-8 my-12">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        {posts.map(post => (
+          <PostCard 
+            key={post.id}
+            number={post.number}
+            title={post.title}
+            body={post.body}
+            created_at={post.created_at}
+          />
+        ))}
+        
       </section>
     </main>
   )
