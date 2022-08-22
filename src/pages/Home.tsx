33125374e8a65b17212a7f190517e791544727c0
Welisson Silveira <welissonmwse@gmail.com>
2022-08-22 10:06:13 -0300

    Conexão com a API - Filtrando Posts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { PostCard } from "../components/PostCard";
 import { Profile } from "../components/Profile";
 import { api } from "../services/api";
@@ -13,7 +13,27 @@ interface PostsDataProsp {
 
 export function Home(){
   const [posts, setPosts] = useState<PostsDataProsp[]>([])
-  // repo:rocketseat-education/reactjs-github-blog-challenge
+  const [issueInput, setIssueInput] = useState('')
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const  response  = await api.get('search/issues', {
+      params: {
+        q: `${issueInput} repo:welissonmwse/github-blog`
+      }
+    })
+    
+    const postsData = response.data.items.map(post => {
+      return {
+        id: post.id,
+        number: post.number,
+        title: post.title,
+        body: post.body,
+        created_at: post.created_at
+      }
+    })
+    setPosts(postsData);
+  }
 
   useEffect(() => {
     (async () => {
@@ -33,7 +53,6 @@ export function Home(){
         }
       })
       setPosts(postsData);
-      console.log(postsData);
     })()
   }, [])
 
@@ -44,10 +63,11 @@ export function Home(){
         <h2 className="text-base-subtitle font-nunito text-lg font-bold">Publicações</h2>
         <p className="font-nunito text-base-span font-normal text-sm">6 publicações</p>
       </header>
-      <form className="mt-3" action="">
+      <form className="mt-3" onSubmit={handleSubmit}>
         <input className="placeholder:font-nunito placeholder:text-base-label py-3 px-4 rounded-md w-full bg-base-input border border-base-border text-base-text focus:border-blue outline-0" 
           type="text" 
           placeholder="Buscar conteúdo" 
+          onChange={(event) => setIssueInput(event.target.value)}
         />
       </form>
       <section className="grid grid-cols-2 gap-8 my-12">
